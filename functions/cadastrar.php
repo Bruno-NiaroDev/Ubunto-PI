51276b5a61e40ab169461934f7159b9831c91da3
Helio Rodrigues <hcrodrigues.filho@gmail.com>
2019-05-04 20:31:51 -0300

    Implementando a funcionalidade de CADASTRO da instituicao!

---
+++ functions/cadastrar.php
@@ -0,0 +1,26 @@
+<?php
+
+if(isset($_POST["btnCadastrar"])){
+
+    include "conexao.php";
+    
+    $cnpj = $_POST['cnpj'];
+    $nomeFantasia = $_POST['nomeFantasia'];
+    $razaoSocial = $_POST['razaoSocial'];
+    $telefone = $_POST['telefone'];
+    $senha = $_POST['senha'];
+    $confirmarSenha = $_POST['confirmarSenha'];
+
+    $sqlInserir = "INSERT INTO instituicao (razaoSocial, cnpj, nomeFantasia, telefone, senha) VALUES ('$razaoSocial', $cnpj, '$nomeFantasia', '$telefone', '$senha')"; 
+    if(mysqli_query($conexao,$sqlInserir) === true){
+        //redirecionar para tela principal do sistema!
+    }else{
+        echo mysqli_error($conexao);
+         
+
+        //header("Location: ../view/cadastro.html?erro=ERRO AO CADASTRAR INSTITUICAO!");
+    }
+}else{
+
+    header("Location: ../view/index.html");
+}
